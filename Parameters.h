1e55b94071117c19df48464a20c103bad54d96a7
mohamed Mansour <67611571+muns99@users.noreply.github.com>
2023-08-07 20:03:29 -0700

    Add files via upload

---
+++ Parameters.h
@@ -0,0 +1,147 @@
+/*
+ * Parameters.h
+ *
+ * Created: 5/20/2023 10:21:51 AM
+ *  Author: Dell
+ */ 
+
+
+#ifndef PARAMETERS_H_
+#define PARAMETERS_H_
+
+
+#define TS 0.01
+/************************************************************************/
+/* PID velocity control                                                                     */
+/************************************************************************/
+f32 VELOCITY_KP = 0.27;
+f32 VELOCITY_KD  = 0.01;
+f32 VELOCITY_KI  = 0.5;
+
+
+/************************************************************************/
+/* PID Position Control                                                                     */
+/************************************************************************/
+#define POSITION_KP 0.12
+#define POSITION_KI 3.5
+#define POSITION_KD 0.2
+
+
+s16 Vmax = 12;
+s16 Vmin = -12;
+f32 v = 0.0;
+
+s16 carPosition  = 0;
+s16 carVelocity  = 0;
+u8  carVelocityIn_M_per_Sec = 0;
+u8 carDir = 'b';
+s16 velovityDerivative = 0.0;
+s16 velocityIntegrator = 0.0;
+s16 velocityError = 0.0;
+s16 velocityPrevError = 0.0;
+
+s16 velocityControlSignal=0;
+s16 velocityPrevControlSignal = 0;
+u16 f_control_signal=0;
+s16 previous_control_signal=0;
+f32 previous_filter_control_signal;
+f32 prev_target_speed = 0;
+s16 targetSpeed = 200;     //target
+s16 targetPosition = -10;  //target
+u8  steeringDir = 'R'; //target
+
+//f32 readings_[numAverage]={0};
+f32 avgFilterReadings[15] = {0} ;
+
+u8 index = 0;
+////////////////
+
+f32 positionDerivative = 0.0;
+f32 positionIntegrator = 0.0;
+f32 positionError = 0.0;
+f32 positionPrevError = 0.0;
+f32 orevInte = 0.0;
+f32 positonControlSignal=0.0;
+
+f32 positonPrevControlSignal=0.0;
+f32 filter_position_control_signal;
+f32 previous_filter_position_control_signal;
+f32 kp_angle=1.3;
+u32 max_steering_angle=30;
+
+f32 Velocity_LPF()
+{
+	return (s16)(0.120198*velocityPrevControlSignal+0.4399*velocityControlSignal+0.4399*velocityPrevControlSignal);
+}
+
+f32 Position_LPF()
+{
+	return (f32)(-0.517093*previous_filter_position_control_signal+0.7585469*positonControlSignal+0.7585469*positonPrevControlSignal);
+}
+
+f64 AverageFilter()
+{
+	s32 Average=0;
+	s32 total=0;
+	total = total-avgFilterReadings[index];
+	avgFilterReadings[index]=carVelocity;
+	total = total + avgFilterReadings[index];
+	index++;
+	if (index>=15)
+	index=0;
+	Average=total/15;
+	return Average;
+}
+
+
+void velocity_PID()
+{
+	
+	velocityError = targetSpeed - carVelocity;
+	velocityIntegrator= velocityIntegrator + velocityError*TS;
+	velovityDerivative = (velocityError-velocityPrevError)/TS;
+	velocityControlSignal=velocityError*VELOCITY_KP+velocityIntegrator*VELOCITY_KI+velovityDerivative*VELOCITY_KD;
+	f_control_signal = velocityControlSignal * 4;
+	
+	velocityPrevError = velocityError;
+	previous_control_signal=f_control_signal;
+	previous_filter_control_signal=f_control_signal;
+	prev_target_speed= targetSpeed;
+	 
+
+}
+
+f32 max(f32 num1,f32 num2)
+{
+	if (num1>num2)
+	return num1;
+	else
+	return num2;
+}
+f32 min(f32 num1,f32 num2)
+{
+	if (num1>num2)
+	return num2;
+	else
+	return num1;
+}
+f32 computeSteering(f32 error)
+{
+	return (max(min(- kp_angle*error,max_steering_angle),- max_steering_angle));
+}
+
+/*void position_PID()
+{
+	f32 npos = (targetPosition* POSITION_PPR) /360.0;
+	positionError=  -(HQUADEn_s32getPositionCount() - npos);
+	positionIntegrator+=positionError*TS;
+	positionDerivative=(positionError-prevPositionError)/TS;
+	positon_control_signal=positionError*POSITION_KP+positionIntegrator*POSITION_KD+positionDerivative*POSITION_KI;
+	filter_position_control_signal=Position_LPF();
+	prevPositionError=positionError;
+	previous_filter_position_control_signal=filter_position_control_signal;
+	previous_positon_control_signal=positon_control_signal;
+	
+}*/
+
+#endif /* PARAMETERS_H_ */
---
+++ main.c
@@ -0,0 +1,186 @@
+/*
+ * CAR.c
+ *
+ * Created: 5/19/2023 6:41:48 AM
+ * Author : MUNSOUR
+ */ 
+#define F_CPU 8000000UL
+#include <util/delay.h>
+#include "include/LIB/STD_TYPES.h"
+#include "Include/LIB/BIT_MATH.h"
+
+#include "Include/MCAL/DIO/DIO_Interface.h"
+#include "Include/MCAL/EXTI/EXTI_Interface.h"
+#include "Include/MCAL/GI/GI_Interface.h"
+#include "Include/MCAL/TIMERS/TIMERS_Interface.h"
+#include "Include/MCAL/TWI/TWI_Interface.h"
+#include "Include/HAL/QUAD_ENCODER/QUAD_ENCODER_Interface.h"
+#include "Include/HAL/LCD_Driver/LCD_Interface.h"
+#include "Include/CAR/CAR.h"
+#include "Parameters.h"
+
+
+void setCar()
+{
+
+	s32 PPS = HQUADEn_s32getVelocityCount() / 0.01; //pulses per second
+	carVelocity = (PPS / 385) * 60; // velocity in rpm
+	carVelocity = carVelocity < 0 ? carVelocity * -1 : carVelocity;
+	carPosition = HQUADEn_s32getPositionCount()  ;
+	
+	
+	u32 circumference = 2 * pi * WHEEL_DIAMETER;
+	carVelocityIn_M_per_Sec = (circumference * carVelocity)/60; //velocity in m/s
+	HQUADEn_voidResetVelocityCount();
+
+
+	velocityError =( targetSpeed - carVelocity);
+	positionError = targetPosition - carPosition;
+	
+	f32 kp = VELOCITY_KP * velocityError;
+	positionDerivative = ((positionError - positionPrevError)/TS) * POSITION_KD;
+	velovityDerivative = ((velocityError - velocityPrevError)/TS) * VELOCITY_KD;
+	
+	velocityIntegrator += (VELOCITY_KI * velocityError * TS);
+	positionIntegrator += (POSITION_KI * positionError* TS);
+	
+	positionPrevError = positionError;  
+	velocityPrevError = velocityError;
+	
+	velocityControlSignal = kp + velocityIntegrator + velovityDerivative;
+	positonControlSignal = POSITION_KP * positionError + positionIntegrator + positionDerivative; 
+	//control_signal = Velocity_LPF();
+	u16 tmp = velocityControlSignal + velocityPrevControlSignal;
+	if (tmp > 0)
+	{
+		setBackMotor(tmp, carDir);
+	}
+	else if(tmp < 0)
+	{
+		tmp *= -1;
+		setBackMotor(tmp , carDir);
+	}
+	else
+	{
+		setBackMotor(0 ,carDir);
+	}
+	velocityPrevControlSignal += velocityControlSignal;
+
+	/*if(abs(positionError) < 5)
+		{
+			setSteeringMotor(0,Vmax);
+			positonControlSignal = 0;*/		
+	if (positonControlSignal > Vmax)  
+	{
+		positonControlSignal = Vmax;
+
+	}
+	else if (positonControlSignal < Vmin)
+	{
+		positonControlSignal = Vmin;
+	}
+	if(abs(positionError <= 5))
+	positonControlSignal = 0.0;
+	
+	setSteeringMotor(positonControlSignal,Vmax);
+	positionPrevError = positionError;
+	
+	
+//	HLCD_voidSendData("CAR speed : ");
+	
+	//HLCD_voidDisplayNumber(HQUADEn_s32getPositionCount());;
+	/*HLCD_voidSendData('n');
+	HLCD_voidDisplayNumber(carPosition);
+	HLCD_voidSendData('t');
+	HLCD_voidDisplayNumber(targetPosition);*/
+	/*HLCD_voidSendData("control signal : ");
+	if (control_signal < 0)
+	{
+		HLCD_voidSendData('-');
+	}
+	HLCD_voidDisplayNumber(control_signal);*/
+}
+
+
+void Routine()
+{
+	targetSpeed = targetSpeed < 400 ? targetSpeed + 50 : targetSpeed;
+	targetPosition = targetPosition > -360? targetPosition - 10 : 0;
+}
+int main(void)
+{
+	
+	//f32 avgFilterReadings[15] = {0} ;
+	MDIO_voidInit();
+	MTIMER0_voidinit();
+	MTIMER1_voidinit();
+	
+	HQUADEN_voidinitPositionEncoder();
+	HQUADEN_voidinitVelocityEncoder();
+	
+	MTIMER0_voidSetCtcCallback(setCar);
+	//MTWI_voidInitSlave(0x55);
+	MEXTI_voidSetCfg(EXTI2,FALLING_EDGE);
+	MEXTI_voidSetCallback(EXTI2,Routine);
+	
+	//
+	
+	MEXTI_voidEnableINT(EXTI0);
+	MEXTI_voidEnableINT(EXTI1);
+	MEXTI_voidEnableINT(EXTI2);
+	MGI_voidEnable();
+	//HQUADEN_voidinitPositionEncoder();
+	
+	//HLCD_voidInit();
+	//MTIMER1_voidSetOcr1aValue(550);
+	//HLCD_voidSendString("HHELLO");
+	
+	//HLCD_voidClearDisplay();
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+
+		//MDIO_voidSetPortValue(DIO_PORTC,(u8)HQUADEn_s32getVelocityCount());
+	
+		//_delay_ms(500);
+		/*if ((targetSpeed/4 == carVelocity) && (targetSpeed <1000))
+		{
+			targetSpeed += 100;
+		}*/
+		/*if(!MDIO_u8GetPinValue(DIO_PORTB,PIN7))
+		
+			{
+				targetSpeed += 100;
+				if (targetSpeed > 1000)
+				{
+					targetSpeed = 1000;
+				}
+				
+				MDIO_voidTogglePin(DIO_PORTA,PIN1);
+			}*/
+		//setCar();
+		
+		/*_delay_ms(1000);
+		MTIMER1_voidSetOcr1aValue(400);
+		_delay_ms(1000);
+		MTIMER1_voidSetOcr1aValue(600);
+		_delay_ms(1000);
+		MTIMER1_voidSetOcr1aValue(800);
+		_delay_ms(1000);
+		MTIMER1_voidSetOcr1aValue(1000);
+		_delay_ms(1000);*/
+		/*targetSpeed = 0;
+		_delay_ms(3000);
+		targetSpeed= 100;
+		_delay_ms(3000);
+		targetSpeed = 150 ;
+		_delay_ms(3000);
+		targetSpeed = 200;
+		_delay_ms(3000);
+		targetSpeed = 250;
+		_delay_ms(3000);*/
+    }
+	
+}
+
